7c0ec03df1cb7fedccaa1a29041848663069dc1a
wiston1568 <wistonanold2@gmail.com>
2025-06-20 19:21:09 +0300

    Delete src directory

--- src/root.rs
+++
@@ -1,8 +0,0 @@
-use colored::*;
-
-pub fn attempt_root() {
-    println!("{}", "⚡ Attempting root (placeholder)...".yellow());
-
-    // Placeholder logic, real rooting logic must be custom per device
-    // Could use Magisk, exploit, or user-assisted
-}
